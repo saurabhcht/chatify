7f9fcef4ddfa312df1e75492c1564aac55c31502
Saurabh Singh <saurabh0405singh@gmail.com>
2026-03-29 18:44:35 +0530

    redeploy the code now

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,11 +42,11 @@ if (ENV.NODE_ENV === "production") {
 
   const frontendPath = path.join(__dirname, "../frontend/dist");
 
-  app.use(express.static(frontendPath));
+  // app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(frontendPath, "index.html"));
+  // });
 }
 
 server.listen(PORT, () => {
